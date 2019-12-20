963ea8aef71ce48f40b3dd5b45ba9f072c0f5c1a
Joao Morgado <joaomorgado0@gmail.com>
2019-12-20 12:27:08 +0000

    Add token context startup and sample on controller

--- KRFHomepage.WebApi/Controllers/Homepage/HomepageController.cs
+++ KRFHomepage.WebApi/Controllers/Homepage/HomepageController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
+using KRFCommon.Context;
 using KRFCommon.Controller;
 using KRFCommon.CQRS.Query;
 using KRFHomepage.Domain.CQRS.Homepage.Query;
@@ -11,12 +12,18 @@ namespace WebApi.Controllers.Homepage
     [Route("Homepage/")]
     public class HomepageController : KRFController
     {
+        IUserContext _user;
+        public HomepageController( IUserContext userContext )
+        {
+            this._user = userContext;
+        }
+
         [HttpGet("GetData")]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof( HomePageOutput[] ))]
         public async Task<IActionResult> GetData(
                 [FromServices] IQuery<HomePageInput, HomePageOutput[]> query )
         {
-            return await this.ExecuteAsyncQuery(new HomePageInput(), query);                
+            return await this.ExecuteAsyncQuery(new HomePageInput(), query);
         }
     }
 }
--- KRFHomepage.WebApi/Startup.cs
+++ KRFHomepage.WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using KRFCommon.Context;
 using KRFHomepage.App.Injection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -20,6 +21,8 @@ namespace KRFHomepage.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            InjectUserContext.InjectContext(services);
+
             services.AddControllers();
 
             services.AddSwaggerGen(option =>
