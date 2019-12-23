b77f019e21fbe8b4b2e18023844c59df5ec5bb55
Joao Morgado <joaomorgado0@gmail.com>
2019-12-23 17:14:36 +0000

    Added Translations controller

---
+++ KRFHomepage.WebApi/Controllers/Translations/TranslationsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using KRFCommon.Context;
+using KRFCommon.Controller;
+using KRFCommon.CQRS.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Translations
+{
+    [ApiController]
+    [Route("Translations/")]
+    public class TranslationsController : KRFController
+    {
+        [HttpGet("{langCode}")]
+
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(string))]
+        public async Task<IActionResult> Get(
+                //[FromServices] IQuery<HomePageInput, HomePageOutput[]> query
+                [FromRoute] string langCode)
+        {
+            //return await this.ExecuteAsyncQuery(new HomePageInput(), query);
+            return await Task.Run(() => this.Ok(langCode));
+        }
+    }
+}
