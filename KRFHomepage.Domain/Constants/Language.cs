03dbbb985f72ec5c57e5ecf33ded1481e973ec0c
Joao Morgado <joaomorgado0@gmail.com>
2020-01-02 10:40:29 +0000

    Remove unused constants

--- KRFHomepage.Domain/Constants/Language.cs
+++ KRFHomepage.Domain/Constants/Language.cs
@@ -2,10 +2,6 @@
 {
     public static class Language
     {
-        public const string AppName_Key = "AppConfiguration:ApiName";
-        public const string TokenIdentifier_Key = "AppConfiguration:TokenIdentifier";
-        public const string TokenKey_Key = "AppConfiguration:TokenKey";
-
         public static string PtCode { get; internal set; }
     }
 }
