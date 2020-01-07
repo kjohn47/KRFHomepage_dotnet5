28d495b7f70a8257531d78996f4b2b43720ee77a
Joao Morgado <joaomorgado0@gmail.com>
2020-01-07 11:27:28 +0000

    Add database Query functions for add translations

--- KRFHomepage.App/DatabaseHelper/TranslationsDatabaseQuery.cs
+++ KRFHomepage.App/DatabaseHelper/TranslationsDatabaseQuery.cs
@@ -1,7 +1,9 @@
-﻿using KRFHomepage.Domain.Database.Homepage;
+﻿using KRFCommon.Database;
+using KRFHomepage.Domain.Database.Common;
 using KRFHomepage.Domain.Database.Translations;
 using KRFHomepage.Infrastructure.Database.DBContext;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,6 +30,138 @@ namespace KRFHomepage.App.DatabaseHelper
                        .ToList()
                 })
                 .ToListAsync();
+        }
+
+        public async Task<QueryCommand> AddNewLanguageAsync( string langCode, string langDescription )
+        {
+            if (!string.IsNullOrEmpty(langCode) && !string.IsNullOrEmpty(langDescription) && langCode.Length == 2)
+            {               
+                try
+                {
+                    if( await this._homepageDBContext.Languages.AnyAsync( x => x.Code.Equals( langCode ) ) )
+                    {
+                        return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Language code already exists" };
+                    }
+
+                    Language newLang = new Language
+                    {
+                        Code = langCode,
+                        Name = langDescription
+                    };
+
+                    await this._homepageDBContext.Languages.AddAsync(newLang);
+                    await this._homepageDBContext.SaveChangesAsync();
+                }
+                catch( Exception ex )
+                {
+                    return new QueryCommand { Result = QueryResult.Error, ResultDescription = ex.Message };
+                }
+
+                return new QueryCommand { Result = QueryResult.Success, ResultDescription = "Success" };
+            }
+            else
+            {
+                return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Missing data or code is too long (max 2 char)" };
+            }
+        }
+
+        public async Task<QueryCommand> AddNewCategoryAsync(string categoryName)
+        {
+            if (!string.IsNullOrEmpty(categoryName))
+            {               
+                try
+                {
+                    if (await this._homepageDBContext.Categories.AnyAsync(x => x.Value.Equals(categoryName)))
+                    {
+                        return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Category already exists" };
+                    }
+
+                    TranslationCategory newCategory = new TranslationCategory
+                    {
+                        Value = categoryName
+                    };
+
+                    await this._homepageDBContext.Categories.AddAsync(newCategory);
+                    await this._homepageDBContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    return new QueryCommand { Result = QueryResult.Error, ResultDescription = ex.Message };
+                }
+
+                return new QueryCommand { Result = QueryResult.Success, ResultDescription = "Success" };
+            }
+            else
+            {
+                return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Missing data" };
+            }
+        }
+
+        public async Task<QueryCommand> AddNewTokenAsync(string token)
+        {
+            if (!string.IsNullOrEmpty(token))
+            {
+                try
+                {
+                    if (await this._homepageDBContext.Tokens.AnyAsync(x => x.Value.Equals(token)))
+                    {
+                        return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Token already exists" };
+                    }
+
+                    Token newToken = new Token
+                    {
+                        Value = token
+                    };
+
+                    await this._homepageDBContext.Tokens.AddAsync(newToken);
+                    await this._homepageDBContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    return new QueryCommand { Result = QueryResult.Error, ResultDescription = ex.Message };
+                }
+
+                return new QueryCommand { Result = QueryResult.Success, ResultDescription = "Success" };
+            }
+            else
+            {
+                return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Missing data" };
+            }
+        }
+
+        public async Task<QueryCommand> AddNewTranslationAsync(string langCode, string category, string token, string translation)
+        {
+            if (!string.IsNullOrEmpty(langCode) && !string.IsNullOrEmpty(category) && !string.IsNullOrEmpty(token) && !string.IsNullOrEmpty(translation))
+            {
+                try
+                {
+                    if (await this._homepageDBContext.Translations.AnyAsync(x => x.LanguageCode.Equals(langCode) && x.TokenValue.Equals(token) && x.TranslationCategoryValue.Equals(category)))
+                    {
+                        return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Translation already exists" };
+                    }
+
+                    Translation newTranslation = new Translation
+                    {
+                        LanguageCode = langCode,
+                        TranslationCategoryValue = category,
+                        TokenValue = token,
+                        Text = translation
+                    };
+
+                    await this._homepageDBContext.Translations.AddAsync(newTranslation);
+                    await this._homepageDBContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    return new QueryCommand { Result = QueryResult.Error, ResultDescription = ex.Message };
+                }
+
+                return new QueryCommand { Result = QueryResult.Success, ResultDescription = "Success" };
+            }
+            else
+            {
+                return new QueryCommand { Result = QueryResult.Error, ResultDescription = "Missing data" };
+            }
         }
     }
 }
