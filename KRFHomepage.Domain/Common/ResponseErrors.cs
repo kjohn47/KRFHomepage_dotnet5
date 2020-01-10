7cc4f953185aa5eac867403342fe2c7d904b833c
Joao Morgado <joaomorgado0@gmail.com>
2020-01-10 12:48:25 +0000

    Added class for error output on cqrs responses

---
+++ KRFHomepage.Domain/Common/ResponseErrors.cs
@@ -0,0 +1,14 @@
+﻿namespace KRFHomepage.Domain.Common
+{
+    public class ResponseErrors<TErrorDetails>
+    {
+        public ResponseErrors(TErrorDetails errorDetails)
+        {
+            this.HasError = true;
+            this.ErrorDetails = errorDetails;
+        }
+
+        public bool HasError { get; private set; }
+        public TErrorDetails ErrorDetails { get; private set; }
+    }
+}
