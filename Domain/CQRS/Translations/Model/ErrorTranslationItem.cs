7d06273179a65c9447d7533f634519d347923c79
Joao Morgado <joaomorgado0@gmail.com>
2021-02-04 00:40:59 +0000

    +Add: Added database models for error translation tables

---
+++ Domain/CQRS/Translations/Model/ErrorTranslationItem.cs
@@ -0,0 +1,9 @@
+﻿namespace KRFHomepage.Domain.CQRS.Translations.Model
+{
+    public class ErrorTranslationItem
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Domain/Database/Translations/ErrorCode.cs
@@ -0,0 +1,13 @@
+﻿namespace KRFHomepage.Domain.Database.Translations
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ErrorCode
+    {
+        [Key]
+        public string Code { get; set; }
+
+        public virtual ICollection<ErrorTranslation> ErrorTranslations { get; set; }
+    }
+}
